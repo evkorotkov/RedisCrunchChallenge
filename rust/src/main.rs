d6cfae7cbc2d1b2886749e88318e8b306adbf690
Yury Solovyov <invokedynamic@yandex.com>
2020-10-05 19:36:35 +0300

    Initial rust Implementation.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,29 +1,35 @@
 use redis::{Commands, RedisError};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::time::SystemTime;
+use csv::Writer;
+use math::round;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Message {
+  index: i32,
+  wday: u8,
+  payload: String,
+  price: f32,
+  user_id: i32,
+
   #[serde(default)]
   total: f32,
-  wday: u8,
-
-  price: f32,
-  index: i32,
-  user_id: i32,
-  payload: String,
 }
 
 impl Message {
-  const discounts: [f32; 7] = [0.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0];
+  const DISCOUNTS: [f32; 7] = [0.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0];
 
   pub fn from_redis(result: Result<Vec<String>, RedisError>) -> Option<Message> {
     match result {
       Ok(payload) => {
-        let encoded = payload.get(1).unwrap();
-
-        match serde_json::from_str(encoded) {
-          Ok(decoded) => decoded,
-          _ => None,
+        match payload.get(1) {
+          Some(encoded) => {
+            match serde_json::from_str(encoded) {
+              Ok(decoded) => decoded,
+              _ => None,
+            }
+          },
+          _ => None
         }
       },
       _ => None,
@@ -31,22 +37,33 @@ impl Message {
   }
 
   pub fn update_discount(&mut self) {
-    let discount = Self::discounts[self.wday as usize] / 100.0;
-    self.total = self.price * (1.0 - discount);
+    let discount = Self::DISCOUNTS[self.wday as usize] / 100.0;
+    self.total = round::half_up((self.price * (1.0 - discount)).into(), 2) as f32;
   }
+
+  pub fn signature(self) -> String {
+    let encoded = serde_json::to_string(&self).unwrap();
+    let digest = md5::compute(encoded);
+    return format!("{:x}", digest);
+  }
+}
+
+fn now() -> String {
+  let elapsed = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+  return format!("{}", elapsed.as_millis());
 }
 
 fn process_events() {
   let client = redis::Client::open("redis://127.0.0.1/").unwrap();
   let mut con = client.get_connection().unwrap();
+  let mut csv_file = Writer::from_path(format!("../output/rust-{}.csv", now())).unwrap();
 
   loop {
     let encoded = con.brpop("events_queue", 5);
     match Message::from_redis(encoded) {
       Some(mut decoded) => {
         decoded.update_discount();
-
-        println!("{:?}", decoded);
+        csv_file.write_record(&[now(), format!("{}", decoded.index), decoded.signature()]);
       },
       None => break,
     }
